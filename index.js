9a62670cc6367d72f9e3fe522bbd261752d2619b
high_byte <email@hanan.beer>
2023-02-27 15:42:51 +0200

    looking dooope

--- index.js
+++ index.js
@@ -35,6 +35,26 @@ function inCameraView(x1, y1, x2, y2) {
     return (x1 >= cameraView[0] && x2 <= cameraView[2]) && (y1 >= cameraView[1] && y2 <= cameraView[3])
 }
 
+// https://www.geeksforgeeks.org/find-two-rectangles-overlap/
+function inCameraView2(x1, y1, x2, y2) {
+    let l1 = { x: x1, y: y1 }
+    let r1 = { x: x2, y: y2 }
+    let l2 = { x: cameraView[0], y: cameraView[1] }
+    let r2 = { x: cameraView[2], y: cameraView[3] }
+
+    // If one rectangle is on left side of other
+    if (l1.x > r2.x || l2.x > r1.x) {
+        return false;
+    }
+
+    // If one rectangle is above other
+    if (r1.y < l2.y || r2.y < l1.y) {
+        return false;
+    }
+
+    return true;
+}
+
 function drawRect(x, y, width, height) {
     ctx.fillRect(x, y, width, height)
 }
@@ -47,6 +67,7 @@ function drawText(text, x, y, size, font) {
 // const menger_offsets = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
 
 const images = [
+    'beans.jpg',
     // me first :)
     'hb.png',
 
@@ -80,6 +101,99 @@ const images = [
     'gm.jpg'
 ]
 
+function lerp(a, b, t){
+    return (1 - t) * a + t * b
+}
+
+// t should be between 0..1
+function pointOnEdge(x1, y1, x2, y2, t) {
+    return [lerp(x1, x2, t), lerp(y1, y2, t)]
+}
+
+function edgeFromSquare(cx, cy, radius, k) {
+    k = k % 4
+
+    if (k == 0) {
+        return [cx - radius, cy - radius, cx + radius, cy - radius]
+    } else if (k == 1) {
+        return [cx + radius, cy - radius, cx + radius, cy + radius]
+    } else if (k == 2) {
+        return [cx + radius, cy + radius, cx - radius, cy + radius]
+    } else { //if (k == 3) {
+        return [cx - radius, cy + radius, cx - radius, cy - radius]
+    }
+}
+
+function makePolygon(cx, cy, radius, sides=3) {
+    // sqrt(2)
+    // radius *= 3 / (sides*sides) + 1
+
+    const angle_offset = 0 //(22.5 * (sides % 2 + 1))
+    const angle_step = 360 / sides
+    let path = []
+    for (let i = 0; i < sides; i++) {
+        // debugger
+        let angle_deg = (angle_step * i + angle_offset)
+
+        // on a circle contained inside the rectangle
+        let angle_rad = angle_deg / 180 * Math.PI
+
+        let x = cx + radius * Math.cos(angle_rad)
+        let y = cy + radius * Math.sin(angle_rad)
+
+        // https://math.stackexchange.com/questions/2740317/simple-way-to-find-position-on-square-given-angle-at-center
+        // let phi = angle_rad
+        // let k = Math.round(2 * phi / Math.PI) & 3
+        // let a = phi - (Math.PI / 2) * Math.round(phi / (Math.PI / 2))
+        // let x = cx + radius * (Math.cos(phi) / Math.cos(a))
+        // let y = cy + radius * (Math.sin(phi) / Math.cos(a))
+
+        // on the rectangle bounds
+        // first need to figure out which side it's on...
+        // let side_idx = (angle_deg / 90) | 0
+        // if (side_idx > 3) debugger
+        // let edge = edgeFromSquare(cx, cy, radius, side_idx)
+        // let [x, y] = pointOnEdge(edge[0], edge[1], edge[2], edge[3], (angle_deg / 90) - side_idx)
+
+        path.push(x)
+        path.push(y)
+    }
+
+    return path
+}
+
+function drawClippedImage(img, cx, cy, size, sides=3) {
+    let radius = size / 2
+    let path = makePolygon(cx, cy, radius, sides)
+
+    // save the unclipped context
+    ctx.save()
+
+    // define the path that will be clipped to
+    ctx.beginPath()
+    ctx.moveTo(path[0], path[1])
+    for (let i = 2; i < path.length; i += 2) {
+        ctx.lineTo(path[i], path[i + 1])
+    }
+    ctx.closePath();   
+
+    // stroke the path
+    // half of the stroke is outside the path
+    // the outside part of the stroke will survive the clipping that follows
+    ctx.lineWidth = 2
+    ctx.stroke()
+
+    // make the current path a clipping path
+    ctx.clip()
+
+    // draw the image which will be clipped except in the clipping path
+    ctx.drawImage(img, cx - radius, cy - radius, size, size)
+
+    // restore the unclipped context (==undo the clipping path)
+    ctx.restore()
+}
+
+
 /*
     menger size is 3 x 3
     hence center is 1 x 1
@@ -90,37 +204,52 @@ const images = [
 */
 function drawMenger_internal(x, y, size, limit, depth = 2) {
     if (depth >= limit || depth * depth > cameraZoom * 450) {
-        return
+        return false
     }
 
     const next_size = size / 3
     const offset = size / 2
     const next_offset = offset / 3
+    // drawRect(x - offset, y - offset, size, size)
 
-    // center square
-    let color = (depth * 20).toString(16)
-    ctx.fillStyle = `#${color}${color}${color}`
+    let has_drawn = false
+    for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+            if (dx == 0 && dy == 0) {
+                continue
+            }
 
-    drawRect(x - offset, y - offset, size, size)
+            has_drawn |= drawMenger_internal(x + (dx * next_size), y + (dy * next_size), next_size, limit, depth + 1)
+        }
+    }
 
-    if (depth * depth < cameraZoom * 200 && inCameraView(x - next_offset, y - next_offset, x + next_offset, y + next_offset)) {
+    // drawRect(x - offset/1.25, y - offset/1.25, size/1.5, size/1.5)
+    // drawRect(x - offset, y - offset, size, size)
+
+    let img = null
+    if (depth * depth < cameraZoom * 200 && inCameraView2(x - next_offset*2, y - next_offset*2, x + next_offset*2, y + next_offset*2)) {
         let img_id = Math.abs(((x * y + x) / 1000 + depth) | 0) % (images.length + 38)
-        let img = document.getElementById('img_' + img_id)
-        if (img)
-            ctx.drawImage(img, x - next_offset, y - next_offset, next_size, next_size)
+        img = document.getElementById('img_' + img_id)
+        if (img) {
+            drawClippedImage(img, x, y, next_size, limit - depth + 2)
+        } else {
+            if (!has_drawn) {
+                // debugger
+                // center square
+                let color = (depth * 20).toString(16)
+                ctx.fillStyle = `#${color}${color}${color}`
+            
+                drawRect(x - offset/1.1, y - offset/1.1, size/1.1, size/1.1)
+            }
+        }
+        has_drawn = true
+            // ctx.drawImage(img, x - next_offset, y - next_offset, next_size, next_size)
     } else {
         // if (depth == 0)
         // console.log(depth, x, y)
     }
 
-    for (let dx = -1; dx <= 1; dx++) {
-        for (let dy = -1; dy <= 1; dy++) {
-            if (dx == 0 && dy == 0)
-                continue
-
-            drawMenger_internal(x + (dx * next_size), y + (dy * next_size), next_size, limit, depth + 1)
-        }
-    }
+    return has_drawn
 }
 
 function drawMenger(size, limit, depth = 0) {
@@ -137,7 +266,7 @@ function draw() {
     setupView()
 
     const size = 30000
-    drawMenger(size, 7)
+    drawMenger(size, 6)
 
     /*
     ctx.fillStyle = "#991111"
